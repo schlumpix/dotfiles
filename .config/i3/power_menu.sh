7aa1c83b001e704214d577d6f365d0f75ec1c3f6
schlumpix <36489978+schlumpix@users.noreply.github.com>
2020-05-25 20:44:38 +0200

    added rofi powermenu

---
+++ .config/i3/power_menu.sh
@@ -0,0 +1,22 @@
+ 
+#!/bin/bash
+
+# small power menu using rofi, i3, systemd and pm-utils
+# (last 3 dependencies are adjustable below)
+# tostiheld, 2016
+
+poweroff_command="systemctl poweroff"
+reboot_command="systemctl reboot"
+logout_command="i3-msg exit"
+hibernate_command="systemctl hibernate"
+suspend_command="systemctl suspend"
+lock_command="betterlockscreen -l dimblur"
+
+# you can customise the rofi command all you want ...
+rofi_command="rofi -theme powermenu"
+
+#options=$'logout\nreboot\npoweroff\nhibernate\nsuspend' 
+options=$'lock\nlogout\nreboot\npoweroff' 
+
+# ... because the essential options (-dmenu and -p) are added here
+eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
